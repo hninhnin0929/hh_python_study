f5b5fc81aaeff722412ba94dadd1763f7c8ffe01
Hnin Hnin <hh@mit.com.mm>
2022-06-21 22:59:42 +0630

    first commit

---
+++ 14_pattern.py
@@ -0,0 +1,75 @@
+#string
+#enumerate
+
+from re import I
+
+
+row = int(input('Please enter your row number:'))
+
+# for i in range(1,row):
+#   for z in range(i):
+#     print('*', end='')
+#   print('')
+
+# print('-----------------------------')
+
+# for i in range(5,row):
+#   for z in range(i,0,-1):
+#     print('*', end='')
+#   print('')
+
+# print('-----------------------------')
+
+# for i in range(1, row):
+#   for j in range(1, i):
+#     print(j, end=' ')
+#   print('')
+
+# print('-----------------------------')
+
+
+# b = 0
+# # reverse for loop from 5 to 0
+# for i in range(row, 0, -1):
+#   b+=1
+#   for j in range(1, i+1):
+#     print(b, end=' ')
+#   print('\r')
+
+# print('-----------------------------')
+
+# num = row
+# # reverse for loop
+# for i in range(row, 0 ,-1):
+#   for j in range(0, i):
+#     print(num, end=' ')
+#   print('\r')
+# print('-----------------------------')
+
+# for i in range(row, 0, -1):
+#   for j in range(0, i+1):
+#     print(j, end=' ')
+#   print('')
+# print('-----------------------------')
+
+# i = 1
+# while i <= row:
+#   j = 1
+#   while j <= i:
+#     print((i * 2 -1), end=' ')
+#     j = j + 1
+#   i = i + 1
+#   print('')
+# print('-----------------------------')
+
+# for i in range(row, 0, -1):
+#   num = i
+#   for j in range(0, i):
+#     print(num, end=' ')
+#   print('\r')
+
+# for i in range(0, row):
+#   for j in range(0, i+1):
+#     print('*', end=' ')
+#   print('\r')
+
